84485f18792242c226c41cf7af0d654835e06da7
Juan-Espino <juanespino@protonmail.com>
2024-06-17 11:02:53 -0700

    mobileview for highlighted article done now desktopview for highlighted article

--- frontend/src/articles/components/highlighted/HighlightedArticle.js
+++ frontend/src/articles/components/highlighted/HighlightedArticle.js
@@ -6,40 +6,6 @@ const HighlightedArticle = (props) => {
 	return (
 		<React.Fragment>
 			<MobileView article={props.article} />
-			<p>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
-				doloremque, laudantium consectetur sit cum fugit blanditiis pariatur,
-				odit in dolorem esse optio amet labore saepe dolorum, molestiae libero
-				et quam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-				autem alias eaque neque perspiciatis sapiente numquam, dolorum fuga
-				deleniti, iure expedita aliquid, veniam molestias architecto nesciunt.
-				Ut veritatis ea alias! Lorem ipsum, dolor sit amet consectetur
-				adipisicing elit. Sequi doloremque, laudantium consectetur sit cum fugit
-				blanditiis pariatur, odit in dolorem esse optio amet labore saepe
-				dolorum, molestiae libero et quam! Lorem, ipsum dolor sit amet
-				consectetur adipisicing elit. Iste autem alias eaque neque perspiciatis
-				sapiente numquam, dolorum fuga deleniti, iure expedita aliquid, veniam
-				molestias architecto nesciunt. Ut veritatis ea alias! Lorem ipsum, dolor
-				sit amet consectetur adipisicing elit. Sequi doloremque, laudantium
-				consectetur sit cum fugit blanditiis pariatur, odit in dolorem esse
-				optio amet labore saepe dolorum, molestiae libero et quam! Lorem, ipsum
-				dolor sit amet consectetur adipisicing elit. Iste autem alias eaque
-				neque perspiciatis sapiente numquam, dolorum fuga deleniti, iure
-				expedita aliquid, veniam molestias architecto nesciunt. Ut veritatis ea
-				alias! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
-				doloremque, laudantium consectetur sit cum fugit blanditiis pariatur,
-				odit in dolorem esse optio amet labore saepe dolorum, molestiae libero
-				et quam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-				autem alias eaque neque perspiciatis sapiente numquam, dolorum fuga
-				deleniti, iure expedita aliquid, veniam molestias architecto nesciunt.
-				Ut veritatis ea alias! Lorem ipsum, dolor sit amet consectetur
-				adipisicing elit. Sequi doloremque, laudantium consectetur sit cum fugit
-				blanditiis pariatur, odit in dolorem esse optio amet labore saepe
-				dolorum, molestiae libero et quam! Lorem, ipsum dolor sit amet
-				consectetur adipisicing elit. Iste autem alias eaque neque perspiciatis
-				sapiente numquam, dolorum fuga deleniti, iure expedita aliquid, veniam
-				molestias architecto nesciunt. Ut veritatis ea alias!
-			</p>
 		</React.Fragment>
 	);
 };
