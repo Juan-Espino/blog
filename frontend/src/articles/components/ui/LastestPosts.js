fb14b9980c41abfb13098c8cca4a166923d51c9c
Juan-Espino <juanespino@protonmail.com>
2024-06-19 15:57:39 -0700

    carousel done now to make each card a link and add logic to change the articles (which articles is the highlighted one and which are in the carousel)

--- frontend/src/articles/components/ui/LastestPosts.js
+++ frontend/src/articles/components/ui/LastestPosts.js
@@ -5,7 +5,31 @@ import styles from "./LastestPosts.module.css";
 const LastestPosts = (props) => {
 	return (
 		<div className={`${styles["lastest-posts-wrapper"]}`}>
-			<Card />
+			<h2>Latest Posts</h2>
+			<div className={`${styles["lastest-posts-card-wrapper"]}`}>
+				<ul className={`${styles["lastest-posts-ul"]}`}>
+					{props.articles.map((article) => (
+						<Card
+							id={article.id}
+							image={article.image}
+							title={article.title}
+							date={article.date}
+							paragraph={article.paragraph}
+						/>
+					))}
+				</ul>
+				<ul className={`${styles["lastest-posts-ul"]}`}>
+					{props.articles.map((article) => (
+						<Card
+							id={article.id}
+							image={article.image}
+							title={article.title}
+							date={article.date}
+							paragraph={article.paragraph}
+						/>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
--- frontend/src/articles/pages/MainArticle.js
+++ frontend/src/articles/pages/MainArticle.js
@@ -14,6 +14,28 @@ const MainArticle = (props) => {
 			image:
 				"https://m.media-amazon.com/images/M/MV5BNmNkNWU5NzUtNmVkNS00ZDE2LTg0NjgtNTIxNWYxOWIyM2FlXkEyXkFqcGdeQWFkcmllY2xh._V1_.jpg",
 		},
+		{
+			id: "a2",
+			title: "No way",
+			date: "12 may, 2020",
+			paragraph: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nobis dolores cumque ab velit molestiae ducimus architecto? Placeat illo dolorem voluptatibus assumenda, odit sint, maxime fugit ut voluptates molestias perferendis. mLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore mollitia, quae ut beatae recusandae ratione provident, quam rerum itaque earum optio quisquam praesentium odit sequi dolore alias nostrum consequatur.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere inventore deleniti voluptates labore dignissimos a, incidunt minus dolorem, atque consequuntur quisquam odit tenetur reprehenderit, error commodi suscipit. Tenetur, sequi cum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore mollitia, quae ut beatae recusandae ratione provident, quam rerum itaque earum optio quisquam praesentium odit sequi dolore alias nostrum consequatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore mollitia, quae ut beatae recusandae ratione provident, quam rerum itaque earum optio quisquam praesentium odit sequi dolore alias nostrum consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore mollitia, quae ut beatae recusandae ratione provident, quam rerum itaque earum optio quisquam praesentium odit sequi dolore alias nostrum consequatur.`,
+			image:
+				"https://st5.depositphotos.com/62628780/63465/i/450/depositphotos_634652768-stock-photo-cant-believe-handsome-african-american.jpg",
+		},
+		{
+			id: "a3",
+			title: "Yo you twiken",
+			date: "12 april, 2020",
+			paragraph: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nobis dolores cumque ab velit molestiae ducimus architecto? Placeat illo dolorem voluptatibus assumenda, odit sint, maxime fugit ut voluptates molestias perferendis. mLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore mollitia, quae ut beatae recusandae ratione provident, quam rerum itaque earum optio quisquam praesentium odit sequi dolore alias nostrum consequatur.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere inventore deleniti voluptates labore dignissimos a, incidunt minus dolorem, atque consequuntur quisquam odit tenetur reprehenderit, error commodi suscipit. Tenetur, sequi cum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore mollitia, quae ut beatae recusandae ratione provident, quam rerum itaque earum optio quisquam praesentium odit sequi dolore alias nostrum consequatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore mollitia, quae ut beatae recusandae ratione provident, quam rerum itaque earum optio quisquam praesentium odit sequi dolore alias nostrum consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore mollitia, quae ut beatae recusandae ratione provident, quam rerum itaque earum optio quisquam praesentium odit sequi dolore alias nostrum consequatur.`,
+			image: "https://i.ytimg.com/vi/AM5qZGjJgCE/sddefault.jpg",
+		},
+		{
+			id: "a4",
+			title: "Naruto",
+			date: "12 oct, 2020",
+			paragraph: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nobis dolores cumque ab velit molestiae ducimus architecto? Placeat illo dolorem voluptatibus assumenda, odit sint, maxime fugit ut voluptates molestias perferendis. mLorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore mollitia, quae ut beatae recusandae ratione provident, quam rerum itaque earum optio quisquam praesentium odit sequi dolore alias nostrum consequatur.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere inventore deleniti voluptates labore dignissimos a, incidunt minus dolorem, atque consequuntur quisquam odit tenetur reprehenderit, error commodi suscipit. Tenetur, sequi cum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore mollitia, quae ut beatae recusandae ratione provident, quam rerum itaque earum optio quisquam praesentium odit sequi dolore alias nostrum consequatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore mollitia, quae ut beatae recusandae ratione provident, quam rerum itaque earum optio quisquam praesentium odit sequi dolore alias nostrum consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore mollitia, quae ut beatae recusandae ratione provident, quam rerum itaque earum optio quisquam praesentium odit sequi dolore alias nostrum consequatur.`,
+			image: "https://facts.net/wp-content/uploads/2023/05/Naruto.jpeg",
+		},
 	];
 	return (
 		<div className={`${styles["main-article"]}`}>
