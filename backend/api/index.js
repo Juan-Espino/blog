0e07e1c0cc6c1aa46df8c6ca976c8a9244d7b90f
Juan-Espino <juanespino@protonmail.com>
2024-07-25 14:35:02 -0700

    trying to fix cors with changing headers

--- backend/api/index.js
+++ backend/api/index.js
@@ -21,7 +21,7 @@ app.use(
 //headers
 app.use((req, res, next) => {
 	//allows any domain to sendz a request
-	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
 
 	res.setHeader(
 		"Access-Control-Allow-Headers",
