d4b733fd3310f5c827ff425e41d24fd2ee682818
Juan-Espino <juanespino@protonmail.com>
2024-07-26 12:08:53 -0700

    changed where files are temporarly stored for deployment to ../../tmp/

--- backend/middleware/file-upload.js
+++ backend/middleware/file-upload.js
@@ -11,7 +11,9 @@ const fileUpload = multer({
 	limits: 500000,
 	storage: multer.diskStorage({
 		destination: (req, file, cb) => {
-			cb(null, "uploads/images");
+			// cb(null, "uploads/images");
+			//production temp folder
+			cb(null, "../../tmp/");
 		},
 		filename: (req, file, cb) => {
 			const ext = MIME_TYPE_MAP[file.mimetype];
