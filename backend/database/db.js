acb0f9a033acf9272cdb82215650f5acaa037843
Juan-Espino <juanespino@protonmail.com>
2025-10-01 14:50:31 -0700

    new aiven database requires a port number rather than the default

--- backend/database/db.js
+++ backend/database/db.js
@@ -6,6 +6,7 @@ const db = mysql.createPool({
 	user: `${process.env.DB_USER}`,
 	password: `${process.env.DB_PASSWORD}`,
 	database: `${process.env.DB_NAME}`,
+	port: `${process.env.DB_PORT}`,
 	connectionLimit: 10,
 });
 
